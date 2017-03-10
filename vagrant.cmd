bc7a84b50f1e181dc7f5196029c107c5970d95fa
hernad <hernad@bring.out.ba>
2017-03-10 20:39:59 +0100

    run cygwin

--- vagrant.cmd
+++ vagrant.cmd
@@ -2,3 +2,8 @@
 REM cscript /cygdrive/c/windows/system32/slmgr.vbs /ato
 
 cscript slmgr.vbs /ato
+
+
+REM C:\cygwin\bin\bash -l "%~f0" "%CD%" %_args%
+
+C:\cygwin\bin\bash -l -c "//vboxsrv/vagrant/build_LO.sh" 
