07783c1ce230d2c31f0e5971154f7292f8e97a82
hernad <hernad@bring.out.ba>
2017-06-20 07:16:48 +0200

    +x

--- vagrant.cmd
+++
@@ -1,9 +0,0 @@
-
-REM cscript /cygdrive/c/windows/system32/slmgr.vbs /ato
-
-cscript slmgr.vbs /ato
-
-
-REM C:\cygwin\bin\bash -l "%~f0" "%CD%" %_args%
-
-C:\cygwin\bin\bash -l -c "//vboxsrv/vagrant/build_LO.sh  --init" 
