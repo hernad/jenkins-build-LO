d7b4d3373d2d1603ef387fad882eed7823116d8c
hernad <hernad@bring.out.ba>
2017-06-20 07:14:29 +0200

    init

--- build_LO.sh
+++ build_LO.sh
@@ -1,70 +1,34 @@
 #!/bin/bash
 
 BRANCH=libreoffice-5-3
-BUILD_HOST="greenbox-5 vagrant W7-vs2013" 
+BUILD_HOST="greenbox-5 vagrant"
 BUILD_ID="hernad $BUILD_HOST"
-
-[ -d /cygdrive/$DRIVE_LETTER ] || DRIVE_LETTER=g
-
-cd /cygdrive/$DRIVE_LETTER/LO
-export LO_HOME=/cygdrive/$DRIVE_LETTER/LO
-export LODE_HOME=$LO_HOME/lode
-export PATH=$LODE_HOME/opt/bin:$PATH
-
-echo $PATH
-
-INIT=0
-[ "$1" == "--init" ] && INIT=1
+LO_TAR_GZ=LO_ubuntu_12.04_i386.tar.gz
 
 
-cd $LO_HOME/libo-core
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -y
+sudo apt-get update -y
+sudo apt-get install -y gcc-4.9 g++-4.9
+sudo update-alternatives --remove-all gcc 
+sudo update-alternatives --remove-all g++
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 20
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
+gcc --version
 
+
+cd /data/LO/core
 git checkout -f $BRANCH
-
-if [ $INIT == 1 ] ; then
-   git pull
-   #git pull
-fi
-
+git pull
 
 
 git checkout -f
 
-perl  -i.original -pe 's/find_msms$/echo no_find_sms/sg'  configure.ac
+#perl  -i.original -pe 's/find_msms$/echo no_find_sms/sg'  configure.ac
 
-#./autogen.sh \
-#    --disable-firebird-sdbc --without-doxygen \
-#    --disable-report-builder --disable-lpsolve --disable-coinmp  --disable-pdfimport  --disable-cve-tests \
-#    --disable-collada \
-#    --without-java --without-help --without-myspell-dicts --disable-odk \
-#    --disable-scripting-beanshell --disable-scripting-javascript --disable-extension-update  --disable-scripting-javascript  \
-#    --enable-release-build --enable-online-update=no \
-#    --disable-report-builder --disable-odk \
-#    --disable-sdremote  --disable-collada   --disable-extension-integration \
-#    --without-help --without-myspell-dicts --disable-sdremote --disable-online-update --without-galleries \
-#    --with-build-version="Built by hernad"  --with-vendor="bring.out" \
-#    --with-extra-buildid="hernad $BUILD_HOST visual studio 2013-cygwin"  \
-#    --with-product-name="LO" CXXFLAGS="/wd4702"   \
-#    --with-locales="bs en" \
-#    --enable-pch --disable-ccache --disable-activex --disable-atl \
-#    --with-lang="bs en-US" 
-
-./autogen.sh \
-    --disable-firebird-sdbc \
-    --enable-release-build \
-    --disable-online-update \
-    --with-build-version="Built by hernad"  --with-vendor="bring.out" \
-    --with-extra-buildid="$BUILD_ID" \
-    --with-product-name="LO" \
-    --with-locales="bs en" \
-   --enable-pch --disable-ccache --disable-activex --disable-atl \
-   --with-lang="bs en-US"  \
-   --with-windows-sdk=7.1A
-
-
-# CXXFLAGS="/wd4702"   \
-#--with-windows-sdk=7.1A  \
-#--without-system-libxml
+./autogen.sh --without-doxygen --disable-gtk3 --disable-gstreamer-1-0
 
 # tests off
 #perl   -i.original -pe 'BEGIN{undef $/;} s/^\$\(call gb.*Cppunit \$\*\)\)\)\)$/\t\@echo notestnotest/smg'  solenv/gbuild/CppunitTest.mk
@@ -78,13 +42,10 @@ perl  -i.original -pe 's/find_msms$/echo no_find_sms/sg'  configure.ac
 
 perl  -i.original -pe 'BEGIN{undef $/;} s/^\$\(call gb_CppunitTest_get_target.*Cppunit \$\*\){3,5}$/\t\@echo notestnotest/smg'  solenv/gbuild/CppunitTest.mk
 
-[ $INIT == 1 ] && $LODE_HOME/opt/bin/make clean
-
-$LODE_HOME/opt/bin/make
-
+make
 
 mv instdir LO
-tar -cvzf LO.tar.gz LO
-cp LO.tar.gz //vboxsrv/vagrant/
-rm LO.tar.gz
+tar -cvzf $LO_TAR_GZ LO
+cp $LO_TAR_GZ //vboxsrv/vagrant/
+rm $LO_TAR_GZ
 
